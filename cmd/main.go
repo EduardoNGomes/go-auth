6f80bd573506d5d8dfae0fe0d9ffaee8e28ae017
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-04-16 23:04:45 -0300

    feat: integrate google oauth and http server setup

--- cmd/main.go
+++ cmd/main.go
@@ -3,38 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	r "gitbhub.com/eduardongomes/go-auth/internal/routes"
+
+	g "gitbhub.com/eduardongomes/go-auth/internal/google"
 	_ "github.com/joho/godotenv/autoload"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
 )
 
 func main() {
-	// Your credentials should be obtained from the Google
-	// Developer Console (https://console.developers.google.com).
-	conf := &oauth2.Config{
-		ClientID:     os.Getenv("CLIENT_ID"),
-		ClientSecret: os.Getenv("CLIENT_SECRET"),
-		RedirectURL:  "YOUR_REDIRECT_URL",
-		Scopes: []string{
-			//"https://www.googleapis.com/auth/userinfor.email",
-			//"https://www.googleapis.com/auth/userinfor.profile",
-			"email",
-			"profile",
-		},
-		Endpoint: google.Endpoint,
-	}
-	// Redirect user to Google's consent page to ask for permission
-	// for the scopes specified above.
-	url := conf.AuthCodeURL("state")
-	fmt.Printf("Visit the URL for the auth dialog: %v", url)
 
-	// Handle the exchange code to initiate a transport.
-	tok, err := conf.Exchange(oauth2.NoContext, "authorization-code")
+	googleInterface := g.NewGoogle()
+	server, err := r.NewServer(googleInterface)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	client := conf.Client(oauth2.NoContext, tok)
-	client.Get("...")
+
+	routes, err := r.NewRoutes(server)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	port := os.Getenv("PORT")
+
+	fmt.Printf("Server is running o port %s", port)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), routes); err != nil {
+		log.Fatalf("Fail to run port 5000 %v", err)
+	}
+
 }
