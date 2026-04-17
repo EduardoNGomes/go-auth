3306f5a4282fafbb89b3a77b0b16d0d11b9664e5
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-04-16 23:04:37 -0300

    feat: add routes and handlers for google authentication

---
+++ internal/routes/routes.go
@@ -0,0 +1,103 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	g "gitbhub.com/eduardongomes/go-auth/internal/google"
+	"gitbhub.com/eduardongomes/go-auth/internal/pages"
+)
+
+type Server struct {
+	http.Handler
+	template      *template.Template
+	googleActions g.GoogleActions
+}
+
+const ContentTypeJSON = "application/json"
+
+var HtmlTemplatePath = pages.GetHtmlTemplate()
+
+func NewServer(actions g.GoogleActions) (*Server, error) {
+	return &Server{
+		googleActions: actions,
+	}, nil
+}
+func NewRoutes(s *Server) (*Server, error) {
+
+	template, err := template.ParseFiles(HtmlTemplatePath)
+
+	if err != nil {
+		return nil, fmt.Errorf("problem loading template %s %v", HtmlTemplatePath, err)
+	}
+
+	s.template = template
+
+	router := http.NewServeMux()
+
+	router.Handle("/home", http.HandlerFunc(s.getPage))
+	fileServer := http.FileServer(http.Dir("internal/pages"))
+
+	router.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	router.Handle("/login", http.HandlerFunc(s.login))
+	router.Handle("/callback", http.HandlerFunc(s.callback))
+	router.Handle("/hc", http.HandlerFunc(s.healthchecker))
+	s.Handler = router
+
+	return s, nil
+}
+
+func (s *Server) healthchecker(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		{
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode("Im breathing")
+
+		}
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Not Found")
+	}
+}
+
+func (s *Server) getPage(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	s.template.Execute(w, nil)
+}
+
+func (s *Server) login(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		url, err := s.googleActions.AuthRedirect()
+
+		if err != nil {
+			http.Error(w, "Failed connect on auth route", http.StatusInternalServerError)
+		}
+
+		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Not Found")
+	}
+}
+
+func (s *Server) callback(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		url, err := s.googleActions.CallbackRedirect(r)
+
+		if err != nil {
+			http.Error(w, "Failed connect on callback route", http.StatusInternalServerError)
+		}
+
+		http.Redirect(w, r, url, http.StatusPermanentRedirect)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Not Found")
+	}
+}
--- internal/routes/routes_test.go
+++ internal/routes/routes_test.go
@@ -1,18 +1,23 @@
-package server_test
+package routes_test
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"gitbhub.com/eduardongomes/go-auth/internal/server"
+	"gitbhub.com/eduardongomes/go-auth/internal/routes"
+
+	g "gitbhub.com/eduardongomes/go-auth/internal/google"
 )
 
 func TestRoutes(t *testing.T) {
-	serverMock := server.NewServer()
+	c := g.NewGoogleMock()
+	s, _ := routes.NewServer(c)
+	serverMock, _ := routes.NewRoutes(s)
 
-	t.Run("[GET] Login route", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/login", nil)
+	t.Run("[GET] HC route", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/hc", nil)
 		response := httptest.NewRecorder()
 
 		serverMock.ServeHTTP(response, request)
@@ -20,7 +25,33 @@ func TestRoutes(t *testing.T) {
 		if response.Code != http.StatusOK {
 			t.Errorf("Expected -> %d\n Receive ->%d", http.StatusOK, response.Code)
 		}
+
+		var body string
+
+		err := json.NewDecoder(response.Body).Decode(&body)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		bodyMsg := "Im breathing"
+
+		if body != bodyMsg {
+			t.Errorf("Expected -> %s\n Receive ->%s", bodyMsg, body)
+		}
+
 	})
+
+	t.Run("[GET] Login route", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/login", nil)
+		response := httptest.NewRecorder()
+
+		serverMock.ServeHTTP(response, request)
+
+		if response.Code != http.StatusTemporaryRedirect {
+			t.Errorf("Expected -> %d\n Receive ->%d", http.StatusTemporaryRedirect, response.Code)
+		}
+	})
+
 	t.Run("[POST] Login route", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodPost, "/login", nil)
 		response := httptest.NewRecorder()
@@ -38,8 +69,8 @@ func TestRoutes(t *testing.T) {
 
 		serverMock.ServeHTTP(response, request)
 
-		if response.Code != http.StatusOK {
-			t.Errorf("Expected -> %d\n Receive ->%d", http.StatusOK, response.Code)
+		if response.Code != http.StatusPermanentRedirect {
+			t.Errorf("Expected -> %d\n Receive ->%d", http.StatusPermanentRedirect, response.Code)
 		}
 	})
 
