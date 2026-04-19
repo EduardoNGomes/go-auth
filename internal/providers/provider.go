f9aaa9994482f27ae8e8f0a0615e5331cf5c3913
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-04-18 22:43:48 -0300

    feat: add GOOGLE and GITHUB provider constants

--- internal/providers/provider.go
+++ internal/providers/provider.go
@@ -2,6 +2,13 @@ package providers
 
 import "net/http"
 
+type Provider string
+
+const (
+	GOOGLE Provider = "GOOGLE"
+	GITHUB Provider = "GITHUB"
+)
+
 type Actions interface {
 	AuthRedirect(*http.Request) (string, error)
 	CallbackRedirect(*http.Request) (string, error)
