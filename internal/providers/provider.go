d4350f46088bd2292e8446ab37bc8506326c7075
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-04-19 18:41:39 -0300

    refactor: extract common OAuth logic into `authCommon` and `callbackCommon` functions

--- internal/providers/provider.go
+++ internal/providers/provider.go
@@ -1,8 +1,15 @@
 package providers
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
+
+	"github.com/google/uuid"
+	"golang.org/x/oauth2"
 )
 
 type Provider string
@@ -12,9 +19,17 @@ const (
 	GITHUB Provider = "GITHUB"
 )
 
+type User struct {
+	Name      string `json:"name"`
+	AvatarUrl string `json:"avatar_url"`
+	Email     string `json:"email"`
+	Location  string `json:"location"`
+}
 type Actions interface {
 	AuthRedirect(*http.Request) (string, error)
 	CallbackRedirect(*http.Request) (string, error)
+	createJWTToken(User) (string, error)
+	getUser(*http.Client) (User, error)
 }
 
 type OAuthOptions map[Provider]Actions
@@ -51,7 +66,7 @@ func NewOAuthOptions() (OAuthOptions, error) {
 			return options, GithuEnvMissingError
 		}
 
-		options[GOOGLE] = NewGoogle()
+		options[GITHUB] = NewGithub()
 	}
 	return options, nil
 }
@@ -64,4 +79,78 @@ func validateEnvs(envs [3]string) bool {
 	}
 
 	return true
+}
+
+func authCommon(c *oauth2.Config, r *http.Request) (string, error) {
+	uuid, err := uuid.NewRandom()
+
+	if err != nil {
+		wrappedErr := fmt.Errorf("error creating code: %w", err)
+		return "", wrappedErr
+	}
+
+	code := uuid.String() + "--" + r.Referer()
+
+	url := c.AuthCodeURL(code)
+
+	return url, nil
+
+}
+
+type callbackCommonParams struct {
+	c *oauth2.Config
+	g Actions
+	r *http.Request
+}
+
+func callbackCommon(p *callbackCommonParams) (string, error) {
+	conf := p.c
+	code := p.r.FormValue("code")
+	referer := strings.Split(p.r.FormValue("state"), "--")[1]
+
+	parsed, err := url.Parse(referer)
+
+	if err != nil {
+		wrappedErr := fmt.Errorf("error on parser referer: %w", err)
+		return "", wrappedErr
+	}
+
+	base := parsed.Scheme + "://" + parsed.Host
+
+	token, err := conf.Exchange(context.Background(), code)
+
+	if err != nil {
+		wrappedErr := fmt.Errorf("error on validate code: %w", err)
+		return "", wrappedErr
+	}
+
+	client := conf.Client(context.Background(), token)
+
+	user, err := p.g.getUser(client)
+
+	if err != nil {
+		return "", err
+	}
+
+	callbackPath := base + os.Getenv("CALLBACK_PATH")
+
+	tokenJWT, err := p.g.createJWTToken(user)
+
+	if err != nil {
+		wrappedErr := fmt.Errorf("error on create JWT TOKEN: %w", err)
+		return "", wrappedErr
+	}
+
+	u, err := url.Parse(callbackPath)
+
+	if err != nil {
+		wrappedErr := fmt.Errorf("error on create JWT TOKEN on Query: %w", err)
+		return "", wrappedErr
+	}
+
+	q := u.Query()
+	q.Set("token", tokenJWT)
+	u.RawQuery = q.Encode()
+
+	return u.String(), nil
 }
